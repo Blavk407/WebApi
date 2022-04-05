213def9340b6c3933f536b3565cb524d4183a03c
user <user@MOBL-06>
2022-04-05 11:46:03 +0500

    Добавьте файлы проекта.

---
+++ WebApplication1/Context.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+        }
+
+        protected Context()
+        {
+        }
+
+        public DbSet<Book> Books => Set<Book>();
+    }
+
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required, MaxLength(100)]
+        public string Author { get; set; } = string.Empty;
+
+        public int CountOfPages { get; set; }
+    }
+}
---
+++ WebApplication1/Controllers/BooksController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class BooksController : ControllerBase
+    {
+        private readonly Context _context;
+
+        public BooksController(Context context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Book>> Post(Book book)
+        {
+            _context.Books.Add(book);
+            await _context.SaveChangesAsync();
+            return Ok(book);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Book>>> Get()
+        {
+            var books = await _context.Books.ToListAsync();
+            return Ok(books);
+        }
+
+    }
+}
